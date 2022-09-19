99d9ef720dcfc46df807d35de5a2440f6986b08c
otinanai1309 <otinanai1309@gmail.com>
2022-09-19 20:35:24 +0300

    URLs and Class-based Views

---
+++ api/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+  path('todos/', views.TodoList.as_view()),  
+]
+
+"""
+We have one route ‘/todos’ as a demonstration. What we want to achieve is that when a request is made to
+localhost:8000/api/todos, you should get a JSON response with the list of todo instances. But how do we achieve
+this?
+The answer is in views.TodoList.as_view().
+In traditional full stack Django projects, views are used to customize what data to send to the HTML templates. In
+our API project, we use class-based generic views (from DRF) to similarly send customized serialized data but this
+time, to the API endpoints instead of to templates.
+views.TodoList is an instance of a class-based generic view. Django’s generic views help us quickly write views
+(without having to write too much repetitive code) to do common tasks like:
+- Display a list of objects, e.g. list of todos.
+- Display detail pages for a single object. E.g. detail page of a todo.
+- Allow users to create, update, and delete objects – with or without authorization.
+"""
--- api/views.py
+++ api/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import TodoSerializer
+from todo.models import Todo
 
 # Create your views here.
+class TodoList(generics.ListAPIView):
+  # ListAPIView requires two mandatory attributes, serializer_class and queryset.
+  # We specify TodoSerializer which we have earlier implemented serializer_class = TodoSerializer
+  serializer_class = TodoSerializer
+  
+  def get_queryset(self):
+    user = self.request.user
+    
+    return Todo.objects.filter(user=user).order_by('-created')
+  
+    """
+    Code explanation
+    We import DRF ’ s generics class of views.
+    We then create TodoList that uses generics.ListAPIView. ListAPIView is a built-in generic class which creates a
+    read-only endpoint for model instances. ListAPIView requires two mandatory attributes which are serializer_class
+    and queryset.
+    When we specify TodoSerializer as the serializer class, we create a read-only endpoint for todo instances. There are
+    many other generic views available and we explore them later.
+    get_queryset returns the queryset of todo objects for the view. In our case, we specify the query set as all todos
+    which match the user. Additionally, we order the todos by the created date i.e. we show the latest todo first. You
+    can customize get_queryset to return the set of todos that you want.
+    """
