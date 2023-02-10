4d5500318962368701804f7acf5612bbf358113f
otinanai1309 <otinanai1309@gmail.com>
2023-02-10 20:40:44 +0200

    Chapter 14: Create Navigation Header Bar

---
+++ frontend/src/components/add-todo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function AddTodo() {
+  return (
+    <div className="App">
+      Add Todo
+    </div>
+  );
+} 
+
+
+export default AddTodo;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter } from 'react-router-dom';
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App/>
+//     </BrowserRouter>
+//   </React.StrictMode>
+
+// // 
+// );
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
   </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
